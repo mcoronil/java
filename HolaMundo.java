c918b690d90ee23fd7121c3b192667ad27caf56c
mcoronil <44465423+mcoronil@users.noreply.github.com>
2018-10-25 12:34:12 +0200

    Create HolaMundo.java

---
+++ HolaMundo.java
@@ -0,0 +1,10 @@
+/*EL primer programa sencillo para hacer en Java. Simplemente hacer que muestre por pantalla "Hola Mundo"
+IMPORTANTE un buen sangrado o tabulado para hacer más facil la lectura del código. Cada vez que se ponga " { " se al darle a 
+intro hay que tabular. */
+
+public class HolaMundo{
+  public static void main(String[] args){
+		
+		System.out.println("¿Hola Mundo!"); //System.ou.pprintln hace que imprima por pantalla lo que vaya entre comillas
+	}
+}
